13371cfcd45d1e175e9c2f82a2850395ff909561
ctauzzi <caysonfantauzzi@gmail.com>
2022-09-16 13:40:59 -0600

    created home portal page for wdd230

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let year = date.getFullYear();
+let currentDate = document.lastModified;
+document.querySelector(".year").textContent = year;
+document.querySelector(".updated").textContent = currentDate;
--- portal.js
+++
@@ -1,6 +0,0 @@
-// SEQUENCE
-
-let date = new Date();
-console.log(date);
-
-let year = date.getFullYear();
