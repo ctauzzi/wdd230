71699976c7ff198d3cf8dad2f693f3cc6f7f1f0c
ctauzzi <caysonfantauzzi@gmail.com>
2022-09-30 13:00:29 -0600

    added lesson 3, tweaked index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let date = new Date();
 let year = date.getFullYear();
 let currentDate = document.lastModified;
-document.querySelector(".year").textContent = year;
-document.querySelector(".updated").textContent = currentDate;
+document.querySelector("span.year").textContent = year;
+document.querySelector("span.updated").textContent = currentDate;
