a90658d843cfe9cc8dade004bb85c58cc6a99418
ctauzzi <caysonfantauzzi@gmail.com>
2022-10-07 18:05:46 -0600

    added chamberr home page

---
+++ chamber/js/script.js
@@ -0,0 +1,19 @@
+const date = new Date();
+const year = date.getFullYear();
+const currentDate = document.lastModified;
+const datefield = document.querySelector(".date");
+const now = new Date();
+
+document.querySelector("span.year").textContent = year;
+document.querySelector("span.updated").textContent = currentDate;
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+const button = document.getElementById("button");
+button.onclick = toggleMenu;
