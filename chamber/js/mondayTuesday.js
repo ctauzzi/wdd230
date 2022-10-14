20029bf07a6103fd25c52e809cae6d7f0bd0bf4e
ctauzzi <caysonfantauzzi@gmail.com>
2022-10-14 12:53:31 -0600

    finished adding content and fixed JS

--- chamber/js/mondayTuesday.js
+++ chamber/js/mondayTuesday.js
@@ -1,8 +1,6 @@
 const myDate = new Date();
-let month = m_names[myDate.getMonth()];
+const parent = document.querySelector(".monday-box");
 
-const parent = document.querySelector("#announcment-box");
-
-if((myDate.getDay == 1) || (myDate.getDay == 2)){
-    
+if((myDate.getDay() == 1) || (myDate.getDay() == 2)){
+    parent.style.display = "block";
 }
