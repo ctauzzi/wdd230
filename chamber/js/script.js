c46d497ca0bef3b342d68308de1614b28bf76e87
ctauzzi <caysonfantauzzi@gmail.com>
2022-11-04 13:05:54 -0600

    added join and thank you pages

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -36,3 +36,14 @@ if((temperature <= 50) && (windSpeed >= 3)){
     const chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temperature*Math.pow(windSpeed,0.16)));
     document.querySelector("#windChill").innerHTML = chill + "&#8457;";
 }
+
+const dateTime = {
+    "month": date.getMonth(),
+    "date": date.getDate(),
+    "year": date.getFullYear(),
+    "hours": date.getHours(),
+    "minutes": date.getMinutes(),
+    "seconds": date.getSeconds()
+};
+
+document.querySelector('#time-loaded').value = dateTime;
