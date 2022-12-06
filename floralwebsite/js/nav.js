d1cf7b6248739f6be89fbcbe6d0decd0312083d1
Cayson Fantauzzi <caysonfantauzzi@gmail.com>
2022-12-06 14:11:05 -0700

    files updated

--- floralwebsite/js/nav.js
+++ floralwebsite/js/nav.js
@@ -9,21 +9,18 @@ function scrollFunction(){
     if (window.outerWidth >= 825){
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.querySelector(".navbar").style.padding = "5px 5%";
-            document.querySelector("#header-title").style.display = "none";
             document.querySelector(".logo-image").style.display = "none";
         }
         else {
             document.querySelector(".navbar").style.padding = "10px 5%";
             document.querySelector(".logo-image").style.height = "250px";
-            document.querySelector("#header-title").style.display = "block";
             document.querySelector(".logo-image").style.display = "block";
             document.querySelector(".nav-bar").style.display = "flex";
     }
     }else if(window.outerWidth > 600 && window.outerWidth < 825){
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.querySelector(".navbar").style.padding = " 3px 5%";
-            document.querySelector(".navbar").style.height = "50px";
-            document.querySelector("#header-title").style.display = "none";
+            document.querySelector(".logo-image").style.height = "75px";
         }
         else {
             document.querySelector(".navbar").style.padding = "10px 5%";
@@ -31,10 +28,6 @@ function scrollFunction(){
         }
     }else{
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.querySelector(".navbar").style.padding = " 2px 5%";
-            document.querySelector("#header-title").style.display = "none";
-        }
-        else {
             document.querySelector(".navbar").style.padding = "10px 5%";
             document.querySelector(".logo-image").style.height = "75px";
         }
