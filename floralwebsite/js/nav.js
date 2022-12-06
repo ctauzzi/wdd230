48b025cc8a1a1ae11bc719a2d89afa66fc7fd7d6
ctauzzi <caysonfantauzzi@gmail.com>
2022-12-05 20:58:37 -0700

    fixed nav js

--- floralwebsite/js/nav.js
+++ floralwebsite/js/nav.js
@@ -9,17 +9,34 @@ function scrollFunction(){
     if (window.outerWidth >= 825){
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.querySelector(".navbar").style.padding = "5px 5%";
-            document.querySelector(".logo-image").style.height = "100px";
+            document.querySelector("#header-title").style.display = "none";
+            document.querySelector(".logo-image").style.display = "none";
         }
         else {
             document.querySelector(".navbar").style.padding = "10px 5%";
             document.querySelector(".logo-image").style.height = "250px";
+            document.querySelector("#header-title").style.display = "block";
+            document.querySelector(".logo-image").style.display = "block";
+            document.querySelector(".nav-bar").style.display = "flex";
     }
     }else if(window.outerWidth > 600 && window.outerWidth < 825){
-        document.querySelector(".navbar").style.padding = " 3px 5%";
-        document.querySelector(".logo-image").style.height="100px";
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.querySelector(".navbar").style.padding = " 3px 5%";
+            document.querySelector(".navbar").style.height = "50px";
+            document.querySelector("#header-title").style.display = "none";
+        }
+        else {
+            document.querySelector(".navbar").style.padding = "10px 5%";
+            document.querySelector(".logo-image").style.height = "150px";
+        }
     }else{
-        document.querySelector(".navbar").style.padding = " 2px 5%";
-        document.querySelector(".logo-image").style.height="75px";
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.querySelector(".navbar").style.padding = " 2px 5%";
+            document.querySelector("#header-title").style.display = "none";
+        }
+        else {
+            document.querySelector(".navbar").style.padding = "10px 5%";
+            document.querySelector(".logo-image").style.height = "75px";
+        }
     }
 }
