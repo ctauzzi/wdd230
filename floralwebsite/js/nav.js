b640135ad84ad54ffe1d3bacf56165e981fed22b
ctauzzi <caysonfantauzzi@gmail.com>
2022-12-12 13:23:28 -0700

    added more content to pages

--- floralwebsite/js/nav.js
+++ floralwebsite/js/nav.js
@@ -1,7 +1,7 @@
 var tglbtn = document.getElementById("nav-toggle");
 var navlst = document.getElementById("nav-list")
 
-tglbtn.addEventListener('click', () => {navlst.classList.toggle("active");});
+tglbtn.addEventListener('click', () => {navlst.classList.toggle("navactive");});
 
 window.onscroll = function() {scrollFunction()};
 
